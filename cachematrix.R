41f9af204f6a812cfbfb4f46e43126057114733d
SMaratkar <SMaratkar@users.noreply.github.com>
2014-08-21 14:58:54 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains the functions to compute the inverse of the square matrix and cache it.
+## Once computed the inverse, it will be cached and will not get computed again.
 
-## Write a short comment describing this function
-
+## This function is to make cache for inverse of the matrix.
 makeCacheMatrix <- function(x = matrix()) {
  
- InvMat <- NULL
- 
- set <- function(y) {
+  InvMat <- NULL
+  
+  set <- function(y) 
+  {
     Mat <<- y
     InvMat <<- NULL
   }
@@ -18,27 +18,25 @@ makeCacheMatrix <- function(x = matrix()) {
   
   list(set = set, get = get,
        setInvMat = setInvMat,
-       getInvMat = getInvMat)  
-  
+       getInvMat = getInvMat)
 }
 
 
-## Write a short comment describing this function
-
+## Function to compute the inverse of the matrix (if not computed)
+## and then cache it. If inverse of the matrix is already cached then return the
+## same.
 cacheSolve <- function(x, ...) {
-  
   ## Return a matrix that is the inverse of 'x'
   InvMat <- x$getInvMat()
   
-  if(!is.null(InvMat)) {
-    message("getting cached data")
+  if(!is.null(InvMat)) 
+  {
+    message("getting cached matrix")
     return(InvMat)
   }
   
   Mat <- x$get()
-  InvMat <- solve(Mat)
+  InvMat <- tryCatch(solve(Mat), error = function(e) ("Failed to compute inverse of the matrix."))
   x$setInvMat(InvMat)
-  
-  InvMat 
-  
+  InvMat
 }
